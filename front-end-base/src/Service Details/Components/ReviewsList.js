54073b09adb775e65db973e7054e77b521c9726f
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-07-31 17:22:56 +0500

    Review holder created complete

---
+++ front-end-base/src/Service Details/Components/ReviewsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReviewHolder from './ReviewHolder';
+function ReviewsList(props) {
+    return (
+        <div>
+            <ReviewHolder/>
+        </div>
+    );
+}
+
+export default ReviewsList;
--- front-end-base/src/Service Details/Components/ServiceReviews.js
+++ front-end-base/src/Service Details/Components/ServiceReviews.js
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import {Divider,Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import './Styles/StyleSheet.css'
+import Rating from '@material-ui/lab/Rating';
+import ReviewsList from './ReviewsList';
 function ServiceReviews(props) {
+    const classes = useStyles();
     return (
         <div>
-            <h1>Service reviwes</h1>
+            <div className={classes.ratingHeaderContainer}>
+                <div className="RatingHeading">
+                    Rating :
+                </div>
+                <div className="RatingStartsContainer">
+                     <Rating name="size-medium" defaultValue={2} />(23)
+                </div>
+
+            </div>
+
+            <Divider /> 
+            <div className={classes.reviewContainer}>
+                <ReviewsList/>
+            </div>
+
         </div>
     );
 }
 
+const useStyles = makeStyles((theme) => ({
+    container:{
+     
+    },
+    ratingHeaderContainer:{
+        paddingBottom:'4%'
+    },
+    reviewContainer:{
+
+    }
+   }));
 export default ServiceReviews;
