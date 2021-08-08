d97333baffbaa15934f1d7aec59a9c134b00172f
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-08-08 23:28:18 +0500

    Revert "trial"
    This reverts commit 38e54276a8ead38aceba20d0e4f1fa261b718bc0.

--- front-end-base/src/Admin Dashborad/Components/ManageProjects/AddNewProjectTab.js
+++ front-end-base/src/Admin Dashborad/Components/ManageProjects/AddNewProjectTab.js
@@ -1,108 +1,179 @@
 import React,{useState} from 'react';
-import {makeStyles} from '@material-ui/core';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-import ProjectBasicInfoForm from './AddNewProjectSteps/ProjectBasicInfoForm'
-import UploadImages from './AddNewProjectSteps/UploadImages';
-import MakePackages from './AddNewProjectSteps/MakePackages';
-import Preview from './AddNewProjectSteps/Preview';
+import { Grid,makeStyles,TextField,Button,Box} from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import TagFacesIcon from '@material-ui/icons/TagFaces';
 
 function AddNewProjectTab(props) {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const steps = getSteps();
+    const classes = useStyles()
+    const [currency, setCurrency] = useState('React Js');
+    const [chipData, setChipData] = useState([])
+    const [keyWordText,setKeyWordText]=useState();
+    const handleChange = (event) => {
+      setCurrency(event.target.value);
+    };
+    const handleDelete = (chipToDelete) => () => {
+      setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+    };
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+    const handelAddBtnEvent=()=>{
+      let tempData = chipData.map((item)=>  item)
+      tempData.push({key:0,label:keyWordText})
+      setChipData(tempData);
+    }
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+    return (
+        <div className={classes.mainContainer}>
+               {/* Main Grid */}
+               <Grid container className={classes.container}>
+                    
+                    <Grid item lg={5} className={classes.formContainer}>
+                        {/* Left side */}
+                        
+                        <div>Fill the form</div>
+                        <form className={classes.root} onSubmitCapture={(e)=>{ 
+                            e.preventDefault();
+                            alert("Implete the submition")
+                            }} autoComplete="on">
 
-  const handleReset = () => {
-    setActiveStep(0);
-  };
+                            {/* Form container */}
+                           <TextField id="filled-basic" label="Project Title" variant="filled" />
+                           
 
-  return (
-    <div className={classes.root}>
-      <Stepper activeStep={activeStep} className={classes.StepperContainer} >
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>All steps completed</Typography>
-            <Button onClick={handleReset}>Reset</Button>
-          </div>
-        ) : (
-          <div >
 
-               <div>
-                     {getStepContent(activeStep)}
-               </div>
-            
-              <div className={classes.bottomButtonContainer}>
-              <Button
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                className={classes.backButton}
-              >
-                Back
-              </Button>
-              <Button variant="contained" color="primary" onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Finish' : 'Save &  Next'}
-              </Button>
-              </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                           <TextField
+                             id="filled-multiline-static"
+                             label="Project description"
+                             multiline
+                             rows={3}  
+                             variant="filled"
+                           />
+
+
+                           <TextField
+                            id="filled-select-currency"
+                            select
+                            label="Select"
+                            value={currency}
+                            onChange={handleChange}
+                            helperText="Please select catagor to which it belongs"
+                            variant="filled"
+                          >
+                            {catagories.map((option) => (
+                              <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                              </MenuItem>
+                            ))}
+                          </TextField>
+                          
+                          <Grid container>
+                                <Grid item lg={8}>
+                                    {/* Key work text field */}
+                                    <TextField id="filled-basic" label="Key word" variant="filled" onChange={(e)=>{
+                                      setKeyWordText(e.target.value)
+                                    }}/>
+                                </Grid>
+                                <Grid item lg={4} >
+                                    {/* Add key word btn */}
+                                    <Button className={classes.addBtn} variant="contained" color="primary" onClick={handelAddBtnEvent}>
+                                      Add
+                                    </Button>
+                                </Grid>
+                          </Grid>
+
+                          <Box style={{overflow: 'auto',height:60, width: '80%',}}>
+                          <Grid container>
+                              <Paper component="ul" className={classes.rootChips}>
+                                  {chipData.map((data) => {
+                                   let icon;
+                           
+                                   return(
+                                     <span key={data.key}>
+                                       <Chip
+                                         icon={icon}
+                                         label={data.label}
+                                         onDelete={handleDelete(data)}
+                                         className={classes.chip}
+                                       />
+                                     </span>
+                                   )
+                                 })}
+                               </Paper>
+                          </Grid>
+                          </Box>
+                                    
+                          
+                        </form>
+                       
+                    </Grid>
+                    <Grid item lg={7} className={classes.imageAndOtherOptionsContainer}>
+                        {/* Other options contianer container */}
+                    </Grid>
+               </Grid>
+
+        </div>
+    );
 }
-function getSteps() {
-  return ['Basic info of project','Upload Images','Make Packages','Preview'];
-}
 
-function getStepContent(stepIndex) {
-  switch (stepIndex) {
-    case 0:
-      return <ProjectBasicInfoForm/>;
-    case 1:
-      return <UploadImages/>;
-    case 2:
-      return <MakePackages/>;
-    case 3:
-      return <Preview/>;
-    default:
-      return 'Unknown stepIndex';
-  }
-}
+const catagories = [
+    {
+      value: 'Java',
+      label: 'Java',
+    },
+    {
+      value: 'React Js',
+      label: 'React Js',
+    },
+    {
+      value: 'React Native',
+      label: 'React Native',
+    },
+  ];
 
 const useStyles = makeStyles((theme)=>({
-   root: {
-    width: '100%',
-  },
-  backButton: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
- 
-  bottomButtonContainer:{
-    marginLeft:'80%'
+    mainContainer:{
+        width:850
+    },
+    container:{
+        width:'120%',
+    },
+    root: {
+        '& > *': {
+          margin: theme.spacing(1),
+          width:330 ,
+        },
+      },
+    formContainer:{
 
-  }
+        // backgroundColor:"blue"
+    }
+      ,
+    imageAndOtherOptionsContainer:{
+       
+        backgroundColor:'gold'
+    },
+    addBtn:{
+        marginLeft:'10%',
+        marginTop:'9%'
+    },
+    keyWordsListContainer:{
+      height:35,
+      width:'90%',
+      backgroundColor:'blue'
+    },
+  rootChips: {
+    display: 'flex',
+    justifyContent: 'start',
+    // flexWrap: 'wrap',
+    listStyle: 'none',
+    padding: theme.spacing(0.5),
+    margin: 0, 
+    
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
 }));
 
 export default AddNewProjectTab;
