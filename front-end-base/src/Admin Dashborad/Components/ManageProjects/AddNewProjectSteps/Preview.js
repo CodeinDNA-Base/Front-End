38e54276a8ead38aceba20d0e4f1fa261b718bc0
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-08-08 21:08:00 +0500

    trial

---
+++ front-end-base/src/Admin Dashborad/Components/ManageProjects/AddNewProjectSteps/Preview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Preview(props) {
+    return (
+        <div>
+            <h1>here we go view the web prevuew</h1>
+        </div>
+    );
+}
+
+export default Preview;
