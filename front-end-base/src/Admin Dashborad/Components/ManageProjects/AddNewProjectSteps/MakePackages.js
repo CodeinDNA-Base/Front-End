31e1646be3bf923cd875103cbc2f9f8f6442ea82
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-08-09 11:50:19 +0500

    resolved issue

---
+++ front-end-base/src/Admin Dashborad/Components/ManageProjects/AddNewProjectSteps/MakePackages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MakePackages(props) {
+    return (
+        <div>
+            <h1>Here we go to make packages</h1>
+        </div>
+    );
+}
+
+export default MakePackages;
