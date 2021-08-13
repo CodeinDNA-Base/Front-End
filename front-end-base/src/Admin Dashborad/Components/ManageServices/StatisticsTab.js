85f4ddda27ce0aa86eedaa272eef7508d88fa5a5
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-08-13 14:35:40 +0500

    finalized statistics

--- front-end-base/src/Admin Dashborad/Components/ManageServices/StatisticsTab.js
+++ front-end-base/src/Admin Dashborad/Components/ManageServices/StatisticsTab.js
@@ -1,11 +1,54 @@
+import { classExpression } from '@babel/types';
+import { Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
-
+import ServiceCard from './StatisticsSubComponents/ServiceCard';
+import ServiceRowCard from './StatisticsSubComponents/ServiceRowCard';
 function StatisticsTab(props) {
+    const classes = useStyles();
     return (
         <div>
-            <h1>This is StatisticsTab</h1>
+            <Grid container>
+                <Grid item xs={2}>
+                    <ServiceCard/>
+                </Grid>
+                <Grid item xs={2}>
+                    <ServiceCard/>
+                </Grid>
+                <Grid item xs={2}>
+                    <ServiceCard/>
+                </Grid>
+                <Grid item xs={2}>
+                    <ServiceCard/>
+                </Grid>
+                <Grid item xs={2}>
+                    <ServiceCard/>
+                </Grid>
+                <Grid item xs={2}>
+                    <ServiceCard/>
+                </Grid>
+            </Grid>
+            <Grid container className={classes.rowList}>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={10}>
+                    <ServiceRowCard/>
+                    <ServiceRowCard/>
+                    <ServiceRowCard/>
+                    <ServiceRowCard/>
+                    <ServiceRowCard/>
+                    <ServiceRowCard/>
+                    
+                </Grid>
+                <Grid item xs={1}></Grid>
+            </Grid>
         </div>
     );
 }
+const useStyles = makeStyles((theme)=>({
+    container:{
 
+    },
+    rowList:{
+        marginTop:"4%"
+    }
+}))
 export default StatisticsTab;
