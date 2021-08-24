c1f8e3b6dde1ee0ac9161edc48c6b5947a14c1ac
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-08-24 12:14:22 +0500

    updated

---
+++ front-end-base/src/Utils/Routing/Routes.js
@@ -0,0 +1,12 @@
+const Home="Home";
+const ServiceDetails="ServiceDetails";
+const searchProject="searchProject";
+const userdashboard="userdashboard";
+const settings="settings";
+const orders="orders";
+const messaging="messaging";
+const vieworder="vieworder";
+const postrequest="postrequest";
+const splash="splash";
+
+export {Home,ServiceDetails,messaging,orders,postrequest,searchProject,settings,userdashboard,vieworder,splash};
---
+++ front-end-base/src/Utils/Routing/Routing.js
@@ -0,0 +1,40 @@
+import * as routeIds from './Routes';
+import store from '../Redux/store';
+
+/**
+ * ->Whatever state we need to decide to either should allow the path switching, we will get that
+ * state from store [Redux]. 
+ * ->What ever API we need to call, for the purpose of setting permission to chage route, will be 
+ * called here.
+ * 
+ */
+
+export const GotTo=(currentPage,destinationPage)=>{
+    var requestedPath = `From_${currentPage}_TO_${destinationPage}`;
+    switch (requestedPath) {
+        case `From_${routeIds.splash}_TO_${routeIds.Home}`: //FROM_Splash_TO_HOME
+            
+                // Define the logic to decide the switching path permision
+            
+            return {
+                isAllowded:true,
+                path:store.getState().Routes.Home.routeUrl 
+            }
+            
+            break;
+        case `From_${routeIds.Home}_TO_${routeIds.ServiceDetails}`: //FROM_HOME_TO_SERVICEDETAILS
+            
+                // Define the logic to decide the switching path permision
+            
+            return {
+                isAllowded:true,
+                path:store.getState().Routes.ServiceDetails.routeUrl
+            }
+
+            break;
+
+    
+        default:
+            break;
+    }
+}
