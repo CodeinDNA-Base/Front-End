7ec2efa08f229775d9b246b45ff5dc71b9c1d9fa
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-08-28 15:21:26 +0500

    Done filter motor

--- front-end-base/src/Admin Dashborad/Components/Support/FilterMotor.js
+++ front-end-base/src/Admin Dashborad/Components/Support/FilterMotor.js
@@ -18,14 +18,16 @@ function FilterMotor(props) {
 
     const requiredMatchPercentage = 100;
 
+
     const [results,setResults]=useState([]);
+
     const dataToFilter = [
         {
             projectTitle:"Project titl 1",
             projectDesc:"This is new project",
             projectService:"Desktop App",
             projectSubService:"React Js",
-            projectEstimatedPrice:40,
+            projectEstimatedPrice:34,
             projectPublishDate:{
                 month:"4",
                 date:"3",
@@ -123,54 +125,98 @@ function FilterMotor(props) {
 
     ]
 
-    const searchKeyPairs = [
+    const handelApply = ()=>{
+        FilterEngine(dataToFilter,results);
+    }
+
+    const [searchKeyPairs,setSearchKeyPairs] =useState([
         {
             withRespectTo:"projectService", //This will contain value by which we have to sort. these must me keys of the data object
             values:['Desktop App'],
-            typeofValue:[] //Array
+            typeofValue:"Array" //Array
         },
         {
             withRespectTo:"projectSubService", 
             values:['React Js'],
-            typeofValue:[] //Array
+            typeofValue:"Array" //Array
         },
         {
             withRespectTo:"projectPublishDate", 
-            typeofValue:{}, //Object
+            typeofValue:"Object", //Object
             values:{
                 month:"4",
                 date:"3",
                 year:"2019",
                 day:"Monday",
             }
-        }
+
+        },
+        {
+            withRespectTo:"projectRatingStars", 
+            typeofValue:"SingleValueInteger", //SingleValueInteger
+            values:3
+        },
+        {
+            withRespectTo:"projectEstimatedPrice", 
+            typeofValue:"Range", //Range : int
+            values:[2,39]
+        },
+
         
-    ]
+    ]);
 
-    const handelApply = ()=>{
 
-        setResults(produce(results,draft=>{
+    const MaintainFilter=(withRespectTo,typeofValue,values)=>
+    {
+        switch (typeofValue) {
+            case "Range":
+
+                break;
+            case "SingleValueInteger":
+
+                break;
+            case "Object":
+
+                break;
+            case "Array":
+
+                break;
+
+            default:
+                alert("Please give right type value for maitaining filter")
+                break;
+        }
+    }
+
+    const FilterEngine = (dataToFilter,results)=>{
+    setResults(produce(results,draft=>{
             draft = dataToFilter.map((dataItem,index_data_to_filter)=>{
-                let flag = true;
                 let numberOfTimesRequired=0;
                 let numberOfTimesMatched=0;
+                console.log("Item : ----------------------")
                 searchKeyPairs.map((searchKeyPairItem,index_key_pair)=>{
-                    // numberOfTimesRequired+=searchKeyPairItem.values.length;
-                      
-                    if(Array.isArray(searchKeyPairItem.values))
+
+                    if(searchKeyPairItem.typeofValue==="Array")
                     {
-                        numberOfTimesRequired++;
+                        numberOfTimesRequired++; //This size must be equal to number of items in below filter array.
                         // This will match in a way that at least one value from array of values should match from all arrays.
+                        let flag = true;
                         for(let i=0;i<searchKeyPairItem.values.length;i++)
                         {
                             if(dataItem[searchKeyPairItem.withRespectTo]===searchKeyPairItem.values[i])
                             {
+                               console.log(`Key: ${searchKeyPairItem.withRespectTo} matched for item index : ${index_data_to_filter+1}`)
                                numberOfTimesMatched++;
+                               flag=false;
                                break;
                             }
                         }
+                        if(flag)
+                        {
+                            console.log(`Key: ${searchKeyPairItem.withRespectTo} Not matched for item index : ${index_data_to_filter+1}`)                
+                        }
                     }
-                    else
+                    else if (searchKeyPairItem.typeofValue==="Object")
                     {
                         // This will match in way that whole values object should be matched.
                         let targetValue ="";
@@ -185,14 +231,44 @@ function FilterMotor(props) {
 
                         if(targetValue===currentValue)
                         {
+                            console.log(`Key: ${searchKeyPairItem.withRespectTo} matched for item index : ${index_data_to_filter+1}`)
                             numberOfTimesMatched++;
+                        }else{
+                            console.log(`Key: ${searchKeyPairItem.withRespectTo} Not matched for item index : ${index_data_to_filter+1}`)
                         }
 
                     }
-                   
-                })
-
-                console.log("Number of times requ:"+numberOfTimesRequired+"Number of times matched"+numberOfTimesMatched)   
+                    else if(searchKeyPairItem.typeofValue==="SingleValueInteger")
+                    {
+                      
+                        numberOfTimesRequired++;
+                       
+                        if(searchKeyPairItem.values===dataItem[searchKeyPairItem.withRespectTo])
+                        {
+                           console.log(`Key: ${searchKeyPairItem.withRespectTo} matched for item index : ${index_data_to_filter+1}`)
+                           numberOfTimesMatched++;     
+                        }
+                        else
+                        {
+                            console.log(`Key: ${searchKeyPairItem.withRespectTo} Not matched for item index : ${index_data_to_filter+1}`)
+                        }
+                    }
+                    else if(searchKeyPairItem.typeofValue==="Range")
+                    {
+                        // [Min,Max]
+                        numberOfTimesRequired++;
+                        if((dataItem[searchKeyPairItem.withRespectTo]>=searchKeyPairItem.values[0]) && (dataItem[searchKeyPairItem.withRespectTo]<=searchKeyPairItem.values[1]))
+                        {
+                            console.log(`Key: ${searchKeyPairItem.withRespectTo} matched for item index : ${index_data_to_filter+1}`)
+                            numberOfTimesMatched++;
+                        }
+                        else
+                        {
+                            console.log(`Key: ${searchKeyPairItem.withRespectTo} Not matched for item index : ${index_data_to_filter+1}`)          
+                        }
+                        
+                    }
+                });
                 const percentage = (parseFloat(numberOfTimesMatched)/parseFloat(numberOfTimesRequired))*100;
                 console.log("Match percentage is : "+percentage);
                 if(percentage>=requiredMatchPercentage)
